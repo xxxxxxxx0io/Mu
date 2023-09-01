0f887c19c41b8016abb3c7a33455ced5d652b916
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-02 01:51:13 +0300

    Update settings.py

--- modules/utils/inline/settings.py
+++ modules/utils/inline/settings.py
@@ -23,7 +23,7 @@ def setting_markup(_):
                 text="🎩 𝐀𝐮𝐭𝐡 𝐔𝐬𝐞𝐫𝐬", callback_data="AU"
             ),
             InlineKeyboardButton(
-                text="🤖 𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫", url=f"https://t.me/og_dakku"
+                text="🤖 𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫", url=f"https://t.me/UP_UO"
             ),
         ],
         [
