f9e4da6a9c8223822732f8c4ba8e1cca76165a4b
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-02 02:07:49 +0300

    Update repo.py

--- plugins/tools/repo.py
+++ plugins/tools/repo.py
@@ -1,3 +1,8 @@
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
+
 from pyrogram import Client, filters
 
 from modules import app
@@ -6,19 +11,35 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 
 @app.on_message(
-    filters.command("مودي")
+    filters.command("سورس")
     & filters.group
     & ~filters.edited & filters.group & ~filters.edited)
-async def help(client: Client, message: Message):
+async def repo(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/dd6c46b812395a1b607e9.jpg",
-        caption=f"""🥀 𐇮 𝑴𝑹𝑨𝑻 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮 @FC_SI 🥀""",
+        caption=f"""🥀 [ωєℓcσмє τσ sσυяcє zє🎸](https://t.me/UI_XB)\n\n[✘ ժᥱ᥎ ๓๑ᴆỿ ✘ 🎸](https://t.me/UP_UO)\n\n[✘ ժᥱ᥎ Ꭹ๑Ꭹ๑  ✘🎸](https://t.me/FC_SI)""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𐇮 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮 🚸 🥀", url=f"https://t.me/UP_UO")
+                        "🥀 sσυяcє sєzαя 🥀", url=f"https://t.me/UI_XB")
+            ],          
+            [
+                    InlineKeyboardButton(
+                        "🥀 𐇮 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮 🥀", url=f"https://t.me/UP_UO")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🥀 𐇮 𝑴𝑹𝑨𝑻 𝑴𝑶𝑫𝒀 𖠮🚸𖠮 آلـۘهہؚيـٰـ‌ُـُ໋۠بـ໋ۘ۠ه 𐇮 🥀", url=f"https://t.me/FC_SI"
+                    ),
+                    InlineKeyboardButton(
+                        "🥀 جروب آلدعمـ 🥀", url=f"https://t.me/UI_OS")
                 ]
             ]
         ),
-    )
+    ) 
+
+# Power By @BikashHalder & @AdityaHalder 
+# Join @BikashGadgetsTech For More Update
+# Join @AdityaCheats For Hack
+# Join Our Chats @Bgt_Chat & @Adityadiscus 
